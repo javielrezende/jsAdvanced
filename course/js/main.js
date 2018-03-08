995d0f63d6de5a091f8b18a108e85f24c7aa947f
Roger Rezende <javielrezende@gmail.com>
2018-03-08 14:04:45 -0300

    Formatando descrições e valores

--- course/js/main.js
+++ course/js/main.js
@@ -15,17 +15,36 @@ function getTotal(list) {
     return total;
 }
 
+
 function setList(list) {
     // Dentro desta variavel ira o cod html que estava na pagina
     // Foram qubradas as linhas para melhor leitura do codigo
     var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
 
     for (var key in list) {
-        table += '<tr><td>' + list[key].desc + '</td><td>' + list[key].amount + '</td><td>' + list[key].value + '</td><td>Edit | Delete</td></tr>';
+        table += '<tr><td>' + formatDesc(list[key].desc) + '</td><td>' + list[key].amount + '</td><td>' + formatValue(list[key].value) + '</td><td>Edit | Delete</td></tr>';
     }
 
     table += '</tbody>';
     document.getElementById("listTable").innerHTML = table;
 }
+
+// Funcao para formatar as descricao dos produtos
+function formatDesc(desc) {
+    var str = desc.toLowerCase(); // Formata o texto da descrição para minusculo
+    str = str.charAt(0).toUpperCase() + str.slice(1); // primeiro formata o primeiro caracter para
+                                                      // maiusculo e depois concatena o restante da palavra.
+    return str;
+}
+
+// Funcao para formatar os valores
+function formatValue(value) {
+    var str = parseFloat(value).toFixed(2) + ""; // Aqui primeiro formatamos para float, apos fixamos que as
+                                                 // casas decimais serao somente de dois numeros e ao concatenar com valor vazio estamos transformando para string novamente.
+    str = str.replace(".", ","); // Aqui troca os pontos por virgula
+    str = "$ " + str;
+    return str;
+}
+
 setList(list);
 console.log(getTotal(list));
